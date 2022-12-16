bae44d3c4135e5c0b3ad649ac5bf3d24171696d4
Matpdev <matheus2ep@gmail.com>
2022-12-16 10:04:46 -0300

    Added new contructors and visiblity system

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,38 @@
 use rltk::{ GameState, Rltk, RGB };
 use specs::prelude::*;
+pub use rect::Rect;
+pub use components::*;
+pub use map::*;
 
-use crate::components::*;
-use crate::map::*;
-use crate::player::*;
+use player::*;
+use visibility_system::VisibilitySystem;
 
 mod components;
 mod map;
 mod player;
 mod rect;
+mod visibility_system;
 
 pub struct State {
-    ecs: World,
+    pub ecs: World,
+}
+
+impl State {
+    fn run_systems(&mut self) {
+        let mut vis = VisibilitySystem {};
+        vis.run_now(&self.ecs);
+        self.ecs.maintain();
+    }
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
 
-        self.run_systems();
         player_input(self, ctx);
+        self.run_systems();
 
-        let map = self.ecs.fetch::<Vec<TileType>>();
-        draw_map(&map, ctx);
+        draw_map(&self.ecs, ctx);
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -33,35 +43,31 @@ impl GameState for State {
     }
 }
 
-impl State {
-    fn run_systems(&mut self) {
-        self.ecs.maintain();
-    }
-}
-
-#[derive(PartialEq, Copy, Clone)]
-pub enum TileType {
-    Wall,
-    Floor,
-}
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
-    let context = RltkBuilder::simple80x50().with_title("Rogue Like Tutorial").build()?;
-    let mut gs = State { ecs: World::new() };
-    gs.ecs.insert(new_map_rooms_and_corriders());
+    let context = RltkBuilder::simple80x50().with_title("Roguelike Tutorial").build()?;
+    let mut gs = State {
+        ecs: World::new(),
+    };
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<Player>();
+    gs.ecs.register::<Viewshed>();
+
+    let map: Map = Map::new_map_rooms_and_corridors();
+    let (player_x, player_y) = map.rooms[0].center();
+    gs.ecs.insert(map);
 
     gs.ecs
         .create_entity()
-        .with(Position { x: 40, y: 25 })
+        .with(Position { x: player_x, y: player_y })
         .with(Renderable {
             glyph: rltk::to_cp437('@'),
             fg: RGB::named(rltk::YELLOW),
             bg: RGB::named(rltk::BLACK),
         })
         .with(Player {})
+        .with(Viewshed { visible_tiles: Vec::new(), range: 8, dirty: true })
         .build();
 
     rltk::main_loop(context, gs)
--- src/player.rs
+++ src/player.rs
@@ -1,18 +1,21 @@
-use super::{xd_idx, Player, Position, State, TileType};
-use rltk::{Rltk, VirtualKeyCode};
+use rltk::{ VirtualKeyCode, Rltk };
 use specs::prelude::*;
-use std::cmp::{max, min};
+use std::cmp::{ max, min };
+use super::{ Position, Player, Viewshed, TileType, State, Map };
 
 pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
-    let map = ecs.fetch::<Vec<TileType>>();
+    let mut viewsheds = ecs.write_storage::<Viewshed>();
+    let map = ecs.fetch::<Map>();
 
-    for (_player, pos) in (&mut players, &mut positions).join() {
-        let destinations_idx = xd_idx(pos.x + delta_x, pos.y + delta_y);
-        if map[destinations_idx] != TileType::Wall {
+    for (_player, pos, viewshed) in (&mut players, &mut positions, &mut viewsheds).join() {
+        let destination_idx = map.xy_idx(pos.x + delta_x, pos.y + delta_y);
+        if map.tiles[destination_idx] != TileType::Wall {
             pos.x = min(79, max(0, pos.x + delta_x));
             pos.y = min(49, max(0, pos.y + delta_y));
+
+            viewshed.dirty = true;
         }
     }
 }
@@ -21,24 +24,25 @@ pub fn player_input(gs: &mut State, ctx: &mut Rltk) {
     // Player movement
     match ctx.key {
         None => {} // Nothing happened
-        Some(key) => match key {
-            VirtualKeyCode::Left | VirtualKeyCode::Numpad4 | VirtualKeyCode::A => {
-                try_move_player(-1, 0, &mut gs.ecs)
-            }
+        Some(key) =>
+            match key {
+                VirtualKeyCode::Left | VirtualKeyCode::Numpad4 | VirtualKeyCode::A => {
+                    try_move_player(-1, 0, &mut gs.ecs)
+                }
 
-            VirtualKeyCode::Right | VirtualKeyCode::Numpad6 | VirtualKeyCode::D => {
-                try_move_player(1, 0, &mut gs.ecs)
-            }
+                VirtualKeyCode::Right | VirtualKeyCode::Numpad6 | VirtualKeyCode::D => {
+                    try_move_player(1, 0, &mut gs.ecs)
+                }
 
-            VirtualKeyCode::Up | VirtualKeyCode::Numpad8 | VirtualKeyCode::W => {
-                try_move_player(0, -1, &mut gs.ecs)
-            }
+                VirtualKeyCode::Up | VirtualKeyCode::Numpad8 | VirtualKeyCode::W => {
+                    try_move_player(0, -1, &mut gs.ecs)
+                }
 
-            VirtualKeyCode::Down | VirtualKeyCode::Numpad2 | VirtualKeyCode::S => {
-                try_move_player(0, 1, &mut gs.ecs)
-            }
+                VirtualKeyCode::Down | VirtualKeyCode::Numpad2 | VirtualKeyCode::S => {
+                    try_move_player(0, 1, &mut gs.ecs)
+                }
 
-            _ => {}
-        },
+                _ => {}
+            }
     }
 }
